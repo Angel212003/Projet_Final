ef1aca16eec0e151f0de6e3502a68422ce18f3c4
Akoua Ange Sophie <kouadioangesophie183@gmail.com>
2025-10-04 14:55:57 +0000

    correction 6

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello Docker + CI/CD!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+// On exporte l'app pour les tests
 module.exports = app;
+
+// On démarre le serveur uniquement si le fichier est exécuté directement
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
